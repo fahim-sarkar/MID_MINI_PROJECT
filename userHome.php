a6634fb32874ca2bd34054b361303819c4a9c77c
fahim-sarkar <fahim_sarkar@hotmail.com>
2020-08-10 13:38:45 +0600

    update

--- userHome.php
+++ userHome.php
@@ -1,14 +1,22 @@
+<?php
+	session_start();
+
+	if(isset($_COOKIE['STATUS'])){
+		if($_COOKIE['STATUS'] == "OK"){
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
+    <title>User's Home Page</title>
 </head>
 <body>
     <form>
         <table>
             <h1>Welcome </h1>
+            <?php echo $_COOKIE["name"];?>
             <tr>
                 <td>
                     <a href="Profile.php">Profile</a>
